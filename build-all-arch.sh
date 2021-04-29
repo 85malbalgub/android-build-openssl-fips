a945de3805ce0ee6a87108c245b48126e5ac957e
Andrew <33009904+ColdDragon@users.noreply.github.com>
2021-04-29 13:09:14 +0900

    Update build-all-arch.sh

--- build-all-arch.sh
+++ build-all-arch.sh
@@ -26,6 +26,8 @@ else
 	SHARED_OPTION=shared
 fi
 
+_ANDROID_NDK="android-ndk-r18b"
+
 OLD_PWD=$(pwd)
 
 set -e
@@ -43,33 +45,39 @@ for arch in ${archs[@]}; do
         "armeabi")
             _ANDROID_TARGET_SELECT=arch-arm
             _ANDROID_ARCH=arch-arm
+	    _ANDROID_API=android-19
             _ANDROID_EABI=arm-linux-androideabi-4.9
             configure_platform="android-armv7" ;;
         "arm64-v8a")
             _ANDROID_TARGET_SELECT=arch-arm64-v8a
             _ANDROID_ARCH=arch-arm64
+	    _ANDROID_API=android-21
             _ANDROID_EABI=aarch64-linux-android-4.9
             #no xLIB="/lib64"
             configure_platform="linux-generic64" ;;
         "mips")
             _ANDROID_TARGET_SELECT=arch-mips
             _ANDROID_ARCH=arch-mips
+	    _ANDROID_API=android-19
             _ANDROID_EABI=mipsel-linux-android-4.9
             configure_platform="android -DB_ENDIAN" ;;
         "mips64")
             _ANDROID_TARGET_SELECT=arch-mips64
             _ANDROID_ARCH=arch-mips64
+	    _ANDROID_API=android-21
             _ANDROID_EABI=mips64el-linux-android-4.9
             xLIB="/lib64"
             configure_platform="linux-generic64 -DB_ENDIAN" ;;
         "x86")
             _ANDROID_TARGET_SELECT=arch-x86
             _ANDROID_ARCH=arch-x86
+	    _ANDROID_API=android-19
             _ANDROID_EABI=x86-4.9
             configure_platform="android-x86" ;;
         "x86_64")
             _ANDROID_TARGET_SELECT=arch-x86_64
             _ANDROID_ARCH=arch-x86_64
+	    _ANDROID_API=android-21
             _ANDROID_EABI=x86_64-4.9
             xLIB="/lib64"
             configure_platform="linux-generic64" ;;
